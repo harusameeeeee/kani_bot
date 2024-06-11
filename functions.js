804cb76a1536225f0adf167cb03ed155c9c6c459
harusameeeeee <nuhsotas24@gmail.com>
2024-06-11 15:50:50 +0900

    語彙追加

--- functions.js
+++ functions.js
@@ -2,13 +2,12 @@ const seedrandom = require('seedrandom');
 const { firstWords, secondWords } = require("./strings/words.js");
 
 function kaniUranai() {
+    // シード付き乱数生成器の初期化
+    const rng = seedrandom();
     let result;
-    if (Math.random() < 1 / 20) {
+    if (rng() < 1 / 20) {
         result = "エビ";
     } else {
-        // シード付き乱数生成器の初期化
-        const rng = seedrandom();
-
         const firstWord = firstWords[Math.floor(rng() * firstWords.length)];
         const secondWord = secondWords[Math.floor(rng() * secondWords.length)];
 
