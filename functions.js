cb8b554c7ded95cad863cf98a1ce6de2f0395e77
harusameeeeee <nuhsotas24@gmail.com>
2024-09-03 16:29:58 +0900

    ランダム性向上試し

--- functions.js
+++ functions.js
@@ -2,22 +2,30 @@ const seedrandom = require('seedrandom');
 const { firstWords, secondWords } = require("./strings/words.js");
 
 function kaniUranai() {
-    // シード付き乱数生成器の初期化
-    const seed = new Date().getTime(); // 時刻とカウンタを組み合わせる
-    const rng = seedrandom(seed);
-    
+    const rng = seedrandom(); // シードを指定しない場合、ランダムな値が生成されます
     let result;
     if (rng() < 1 / 50) {
         result = "アルティメットハイパーシャイニングミラクルゴッドカニ";
     } else if (rng() < 1 / 20) {
         result = "エビ";
     } else {
-        const firstWord = firstWords[Math.floor(rng() * firstWords.length)];
-        const secondWord = secondWords[Math.floor(rng() * secondWords.length)];
+        const shuffledFirstWords = shuffleArray([...firstWords]);
+        const shuffledSecondWords = shuffleArray([...secondWords]);
+
+        const firstWord = shuffledFirstWords[Math.round(rng() * firstWords.length)];
+        const secondWord = shuffledSecondWords[Math.round(rng() * secondWords.length)];
 
         result = `${firstWord}${secondWord}カニ`;
     }
     return `あなたは   **${result}**   です`;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 module.exports = {
